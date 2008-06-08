b687ca31a0cc7115925ead4a4f0ebc2db2ecc683
oscar.lindberg <oscar.lindberg@aa52d404-074d-0410-be21-9d7082c0964b>
2008-06-08 21:14:25 +0000

    Added file TestCaseHands.txt, which contains the first 10 max score test cases

--- mahtml.py
+++ mahtml.py
@@ -71,6 +71,8 @@ def getAnswerOrError(in_line):
         f = H3("Error")+TEXT(str(e))
     return str(DIV(indata_fragment+f))
 
+# Should be placed in maxpoints.py
+
 def optimal_claimed_fans(exclusion_fans):
     NOT_CLAIMED = 0
     CLAIMED = 1
@@ -84,6 +86,14 @@ def optimal_claimed_fans(exclusion_fans):
                 claimed_fans[j] = IMPOSSIBLE
     return [i for i, v in enumerate(claimed_fans) if v == CLAIMED]
         
+def score_of_claimed(exclusion_fans, claimed_fans):
+    total = 0
+    for i in claimed_fans:
+        score, name, sets, implied, identical, exceptions, accountOnce = exclusion_fans[i]
+        total += score
+    return total
+        
+
 
 def getOptionFragment(i, option):
     this_fragment = H4('Grouping %d' % (i+1)) + getSituationFragment(option)
@@ -96,7 +106,8 @@ def getOptionFragment(i, option):
                                   "Implied", "Identical", "Exceptions", "Account Once"],
                                   exclusion_fans)
     claimed_fans = optimal_claimed_fans(exclusion_fans)
-    this_fragment += PRE(pprint.pformat(claimed_fans))
+    this_fragment += PRE("Claimed (non-account once) fans: " + pprint.pformat(claimed_fans))
+    this_fragment += PRE("Total points: " + str(score_of_claimed(exclusion_fans, claimed_fans)) )
     return this_fragment
 
 def getAnswer(in_line):
