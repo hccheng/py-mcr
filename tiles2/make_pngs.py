67dd9c3ad77224aa584f486488ccafaf057f61b8
oscar.lindberg <oscar.lindberg@aa52d404-074d-0410-be21-9d7082c0964b>
2008-09-19 16:37:04 +0000

    Added new tile graphics.

---
+++ tiles2/make_pngs.py
@@ -0,0 +1,15 @@
+import os
+from PIL import Image
+
+for root, dirs, files in tuple(os.walk('.')):
+    files = [f[:-4] for f in files if f.endswith('.gif')]
+    files = [f for f in files if not f.endswith('r')]
+
+    for image_name in files:
+        image = Image.open(image_name+".gif")
+        transparency = image.getpixel((0, 0))
+        image.save(image_name+".png", "PNG", transparency=transparency)
+        image = image.rotate(270)
+        image.save(image_name+"r.png", "PNG", transparency=transparency)
+        
+
